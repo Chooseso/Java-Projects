65858aca72227788ece482fa3bd6a1af46a343f6
John Roy Calm <calmj55@gmail.com>
2025-03-17 18:53:44 +0800

    Java Project

---
+++ Operators_Arithmetic_Relational/modulusOperatorJRC.java
@@ -0,0 +1,17 @@
+public class modulusOperatorJRC{
+    public static void main(String [] args){
+
+        int num1 = 3;
+        int num2 = 10;
+
+
+        if(num2 % num1 == 0){
+            System.out.println("Divisible by each other");
+        }
+
+        else{
+            System.out.println("Not Divisible by each other");
+        }
+
+    }
+}
---
+++ Operators_Arithmetic_Relational/relationalOperatorsJRC.java
@@ -0,0 +1,24 @@
+public class relationalOperatorsJRC{
+
+    public static void main(String [] args){
+        int x = 10;
+        int y = 15;
+
+        System.out.println(x == y);
+        System.out.println(x != y);
+        System.out.println(x > y);
+        System.out.println(x < y);
+        System.out.println(x >= y);
+        System.out.println(x <= y);
+        
+        // Bonus Task
+        if(x >= 10 && x <= 50){
+            System.out.println("Value of X: " + x + " " + "is Within the range of 10 upto 50");
+        }
+
+
+
+    }
+
+
+}
